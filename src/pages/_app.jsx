5ba56ec9b7975e10a92843283cf901c249c0de84
Manuel Cruz <amcp.negocios@gmail.com>
2024-09-16 21:37:31 -0600

    Updated and added _app.jsx top have global styles

---
+++ src/pages/_app.jsx
@@ -0,0 +1,7 @@
+import '../styles/globals.css'; // Aquí importas tus estilos globales
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
