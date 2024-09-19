041db256163f52d30035cecab51d71ee61bc3419
Molar <grubenmj@gmail.com>
2024-09-18 21:37:31 -0600

    "feat: The buttons are almost ready just need an adjustment in the images and the comfortaa font was added in the tailwind the golbal.css were updated whit the comfortaa font and the tailwind.config the comfortaa font was added and a valible named backround was removed sience it was another one whit the same name"

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,45 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils"
+import { Icon } from "lucide-react";
+
+const buttonVariants = cva(
+  "items-center inline-flex whitespace-nowrap rounded-lg text-baseM font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 md:text-base border-2 gap-2",
+  {
+    variants: {
+      variant: {
+        default: "hover:bg-lightAccent hover:border-lightAccent text-white font-medium bg-accent border-accent",
+        destructive:
+          "hover:bg-red-400 hover:border-red-400 text-white font-medium bg-red-500 border-red-500",
+        outline:
+          "hover:bg-accentBackground hover:border-lightAccent text-accent font-medium bg-background border-accent",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        lg: "h-11 px-8",
+        full:"h-11 px-8 w-full",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+      before:"default"
+    },
+  }
+)
+
+const Button = React.forwardRef(({ className, variant, size, before , asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button"
+  return (
+    (<Comp
+      className={cn(buttonVariants({ variant, size, className, before }))}
+      ref={ref}
+      {...props} />)
+  );
+})
+Button.displayName = "Button"
+
+
+export { Button, buttonVariants }
