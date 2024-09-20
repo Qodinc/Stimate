567450a771872d050bb03a027749393c1f17ca25
Omar D. Sampayo <omar.1993.ds@hotmail.com>
2024-09-20 00:04:39 -0600

    login created wtih responsive format

---
+++ src/pages/login.jsx
@@ -0,0 +1,32 @@
+import Input from "@/components/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Login() {
+    return (
+        <div className="flex w-full min-h-screen md:relative justify-center items-center">
+            <div className="w-full h-screen hidden md:block lg:p-4">
+                <img className="opacity-40 lg:opacity-100 lg:rounded-xl lg:h-full object-cover w-full h-screen" src="imgLogin.jpg" alt="LoginImg" />
+            </div>
+            <div className="flex bg-white font-comfortaa md:absolute lg:static md:py-20 md:max-w-96 lg:min-w-[50%] lg:rounded-none md:rounded-xl flex-col items-center justify-center w-full px-4 lg:px-12 gap-8">
+                <h1 className="font-bold text-2xl text-accent" >Bienvenido!</h1>
+                <form className="flex flex-col gap-4 w-full" action="#">
+                    <div className="flex flex-col gap-2">
+                        <span>Correo Electrónico</span>
+                        <Input placeholder="Correo Electrónico" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <span>Contraseña</span>
+                        <Input placeholder="Contraseña" type="password" />
+                    </div>
+                    <div className="flex justify-end items-center">
+                        <Link className="text-accent" href="#">Olvidaste tu contraseña?</Link>
+                    </div>
+                    <div className="flex justify-center items-center py-8">
+                        <Button variant="default" size="default" >Iniciar Sesión</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+  }
