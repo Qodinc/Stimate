24a99116ce023d9321632021a68461091792e2ca
Molar14 <grubenmj@gmail.com>
2024-09-24 01:37:51 -0600

    "feat: the button in the alert dialog was updated and now recive variants and size, the button was updated and now the content its centered in the full size view and the alerts-variants file was made and the alerts were made to be ussed (pending to be reviewed) "

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -3,7 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 const buttonVariants = cva(
-  "items-center inline-flex whitespace-nowrap rounded-lg text-baseM font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 md:text-base border-2 gap-2",
+  "items-center inline-flex whitespace-nowrap rounded-lg text-baseM font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 md:text-base border-2 gap-2 justify-center",
   {
     variants: {
       variant: {
