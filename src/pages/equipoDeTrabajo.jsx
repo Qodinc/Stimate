0cd450e84d62c6084f78c2008871e45d7131e462
Santiago Yañez <angel.yaez2003@gmail.com>
2024-09-24 22:03:25 -0600

    Add branch Equipo de trabajo

---
+++ src/pages/equipoDeTrabajo.jsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar"
+import Save from "@/components/Icons/Save"
+
+
+
+
+
+export default function EquipoDeTrabajo () {
+    return (
+        <>
+        <Navbar  />
+        <div>
+            <div className="pl-6">
+               <Save 
+            width={24}
+            height={24}
+            /> 
+            </div>
+            
+    
+        </div>
+        </>
+       
+    )
+}
