7e853229b103a27af236c6e8d65a1ea3ebbee489
Stiglitz <hgogzzs@gmail.com>
2024-09-24 23:14:08 -0600

    Se añaden componentes para iconos svgs y se hace prueba de preview como componente usando tabsMenu

--- src/components/tabContent/Preview.jsx
+++ src/components/tabContent/Preview.jsx
@@ -1,11 +1,25 @@
+import ExportDownload from "../Icons/ExportDownload";
+import Percent from "../Icons/Percent";
+import Input from "../input";
+import { Button } from "../ui/button";
+
 
 const Preview = () => {
     return (
-      <div>
-        <h2>Previsualisacion</h2>
-        <p>contenido de previsualizacion</p>
-        {/* Contenido específico de las funcionalidades */}
-      </div>
+      <main className="m-2 mt-5">
+        <div className="flex justify-end">
+          <Button>Exportar <ExportDownload width={25} height={25}  /></Button>
+        </div>
+        <div>
+          <h2 className="font-poppins text-lg font-semibold text-accent mb-3">Datos adicionales</h2>
+          <div>
+            <p className="font-comfortaa text-base text-baseColor">Comisión por venta</p>
+            <Input icon={<Percent width={20} height={20}  />} iconPosition="right"  />
+
+            <Input />
+          </div>
+        </div>
+      </main>
     );
   };
   
