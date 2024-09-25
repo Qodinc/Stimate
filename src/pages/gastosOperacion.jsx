a6e42d8192c0c396b0153e3d5c19675c0f606741
Omar D. Sampayo <omar.1993.ds@hotmail.com>
2024-09-24 22:14:54 -0600

    :3

--- src/pages/gastosOperacion.jsx
+++ src/pages/gastosOperacion.jsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/Navbar";
 import CardGastosOperacion from "@/components/cardGastosOperacion";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
@@ -12,17 +11,14 @@ export default function GastosOperacion() {
     setCards([...cards, { id: cards.length + 1 }]);
     };
     return (
-        <div className="w-full justify-center items-center flex flex-col gap-4">
-            <Navbar />
-            <div className="w-full justify-center items-center flex flex-col gap-8 p-8">
-                <div id="contentCard" className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 gap-4 w-full justify-center items-center">
-                    {cards.map((card) => (
-                            <CardGastosOperacion key={card.id} cardID={card.id} />
-                        ))}
-                </div>
-                <div className="flex justify-start items-center w-full">
-                    <Button variant="default" size="default" onClick={addCard}>Agregar gasto</Button>
-                </div>
+        <div className="w-full justify-center items-center flex flex-col gap-8 p-8">
+            <div id="contentCard" className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 gap-4 w-full justify-center items-center">
+                {cards.map((card) => (
+                        <CardGastosOperacion key={card.id} cardID={card.id} />
+                    ))}
+            </div>
+            <div className="flex justify-start items-center w-full">
+                <Button variant="default" size="default" onClick={addCard}>Agregar gasto</Button>
             </div>
         </div>
     );
