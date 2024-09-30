c7aee1d8e8b76135de41e761c40863f61c15629a
Santiago Yañez <angel.yaez2003@gmail.com>
2024-09-29 22:05:36 -0600

    Changes

--- src/pages/equipoDeTrabajo.jsx
+++ src/pages/equipoDeTrabajo.jsx
@@ -5,9 +5,25 @@ import Timer from "@/components/Icons/Timer";
 import Dinero from "@/components/Icons/DollarSign";
 import { Button } from "@/components/ui/button";
 import Plus from "@/components/Icons/Plus";
-import { Card,CardVariants } from "@/components/cardArea";
+import { Card, CardVariants } from "@/components/cardArea";
+import React, { useState } from "react";
 
 export default function EquipoDeTrabajo() {
+  const [cardData, setCardsData] = useState([]);
+
+  const handleAddCard = () => {
+    const newCardData = {
+      areaName: "",
+      sueldoHora: "",
+      sueldoMes: "",
+      horasDias: "",
+      horasSemana: "0",
+      horasMes: "0",
+    };
+
+    setCardsData([...cardData, newCardData]);
+  };
+
   return (
     <>
       <Navbar />
@@ -34,63 +50,82 @@ export default function EquipoDeTrabajo() {
           </button>
         </div>
 
-        <Card size="lg" className="grid grid-cols-2 p-2 justify-items-center sm:grid-cols-3 lg:grid-cols-6 lg:h-24">
-          <div  className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
-            <span className="font-comfortaa text-base">Nombre del área</span>
-            <Input placeholder="Agregar área" required={true} icon={<Timer width={24} />} />
-          </div>
-          <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
-            <span className="font-comfortaa text-base">Sueldo por hora</span>
-            <Input
-              placeholder="Agregar sueldo"
-              iconPosition="left"
-              type="number"
-              icon={<Dinero width={24} />}
-            />
-          </div>
-          <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
-            <span className="font-comfortaa text-base">Sueldo por mes</span>
-            <Input
-              placeholder="Agregar sueldo"
-              iconPosition="left"
-              type="number"
-              icon={<Dinero width={24} />}
-            />
-          </div>
-          <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
-            <span className="font-comfortaa text-base">Horas al día</span>
-            <Input
-              placeholder="Agregar horas"
-              iconPosition="left"
-              type="number"
-              icon={<Timer width={24} />}
-            />
-          </div>
-          <div className="sm:w-[200px] lg:h-12 m-2 w-[155px] ">
-            <span className="font-comfortaa  md:text-base text-[15px]">
-              Horas a la semana
-            </span>
-            <Input
-              placeholder="0"
-              iconPosition="left"
-              type="number"
-              disabled={true}
-              icon={<Timer width={24} />}
-            />
-          </div>
-          <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
-            <span className="font-comfortaa text-base">Horas al mes</span>
-            <Input
-              placeholder="0"
-              iconPosition="left"
-              type="number"
-              disabled={true}
-              icon={<Timer width={24} />}
-            />
-          </div>
-        </Card>
+        <div className="flex flex-col gap-5 py-5">
+          {cardData.map((cardData, index) => (
+            <Card
+              size="lg"
+              className="grid grid-cols-2 p-2 justify-items-center sm:grid-cols-3 lg:grid-cols-6 lg:h-24 shadow-lg"
+              key={index}
+            >
+              <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
+                <span className="font-comfortaa text-base">
+                  Nombre del área
+                </span>
+                <Input
+                  placeholder="Agregar área"
+                  icon={<Timer width={24} />}
+                  value={cardData.areaName}
+                  onChange={(e) => {
+                    cardData.areaName = e.target.value;
+                  }}
+                />
+              </div>
+              <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
+                <span className="font-comfortaa text-base">
+                  Sueldo por hora
+                </span>
+                <Input
+                  placeholder="Agregar sueldo"
+                  iconPosition="left"
+                  type="number"
+                  icon={<Dinero width={24} />}
+                />
+              </div>
+              <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
+                <span className="font-comfortaa text-base">Sueldo por mes</span>
+                <Input
+                  placeholder="Agregar sueldo"
+                  iconPosition="left"
+                  type="number"
+                  icon={<Dinero width={24} />}
+                />
+              </div>
+              <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
+                <span className="font-comfortaa text-base">Horas al día</span>
+                <Input
+                  placeholder="Agregar horas"
+                  iconPosition="left"
+                  type="number"
+                  icon={<Timer width={24} />}
+                />
+              </div>
+              <div className="sm:w-[200px] lg:h-12 m-2 w-[155px] ">
+                <span className="font-comfortaa  md:text-base text-[15px]">
+                  Horas a la semana
+                </span>
+                <Input
+                  placeholder="0"
+                  iconPosition="left"
+                  type="number"
+                  disabled={true}
+                  icon={<Timer width={24} />}
+                />
+              </div>
+              <div className="sm:w-[200px] lg:h-12 m-2 w-[155px]">
+                <span className="font-comfortaa text-base">Horas al mes</span>
+                <Input
+                  placeholder="0"
+                  iconPosition="left"
+                  type="number"
+                  disabled={true}
+                  icon={<Timer width={24} />}
+                />
+              </div>
+            </Card>
+          ))}
+        </div>
         <div className="mt-[40px]">
-          <Button>
+          <Button onClick={handleAddCard}>
             <Plus width={24} stroke="white" />
             Agregar área
           </Button>
