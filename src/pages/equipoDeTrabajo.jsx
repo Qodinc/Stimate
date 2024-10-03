7b83615b00d69bbb7f3201aadb8293f4c91c0f27
Carlos Jimenez <iicarlosjim@gmail.com>
2024-10-03 00:08:12 -0600

    fix: Equipo de trabajo Modal Eliminar

--- src/pages/equipoDeTrabajo.jsx
+++ src/pages/equipoDeTrabajo.jsx
@@ -77,28 +77,6 @@ export default function EquipoDeTrabajo() {
     <>
       <Navbar />
       <div className="flex-row h-[85px] px-4 md:px-14 lg:px-20">
-        <div className="cursor-pointer">
-          <Save width={24} height={24} />
-        </div>
-
-        <div className="bg-[#DEDCFF] pb-0 rounded-t-[12px] rounded-b-none flex overflow-x-auto scrollbar-hide w-6/12 font-comfortaa">
-          <button className="p-1 ml-2 mt-1 rounded-t-[12px] rounded-b-none flex-shrink-0 bg-white ">
-            Equipo de trabajo
-          </button>
-          <button className="p-1 ml-2 mt-1 rounded-t-[12px] rounded-b-none flex-shrink-0">
-            Funcionalidades
-          </button>
-          <button className="p-1 ml-2 mt-1 rounded-t-[12px] rounded-b-none flex-shrink-0">
-            Gastos de operacion
-          </button>
-          <button className="p-1 ml-2 mt-1 rounded-t-[12px] rounded-b-none flex-shrink-0">
-            Cargos Asociados
-          </button>
-          <button className="p-1 ml-2 mt-1 rounded-t-[12px] rounded-b-none flex-shrink-0">
-            Previsualizacion
-          </button>
-        </div>
-
         <div className="flex flex-col gap-5 py-5">
           {cardData.map((cardData, index) => (
             <Card
@@ -185,7 +163,7 @@ export default function EquipoDeTrabajo() {
 
               <div className="col-start-2 mt-3 sm:col-start-3 lg:col-start-6 ml-6">
                 <AlertDialog>
-                  <AlertDialogTrigger>
+                  <AlertDialogTrigger asChild>
                     <Button>
                       <Trash width={24} stroke="white" />
                       Eliminar
