f050c389b93555eab7b9428fef5768b851354d89
Carlos Jimenez <iicarlosjim@gmail.com>
2024-10-11 20:53:00 -0600

    fix: Suscripción
    * Se cambia la división de componentes
    * se reparan los enviroments

--- src/pages/suscripcion.jsx
+++ src/pages/suscripcion.jsx
@@ -1,7 +1,4 @@
-import Input from "@/components/input";
-import Navbar from "@/components/Navbar";
-import { Button } from "@/components/ui/button";
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import {
    Elements,
    useStripe,
@@ -11,10 +8,14 @@ import {
    CardCvcElement
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import Input from "@/components/input";
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
 
 export default function Payment() {
    const [stripePromise, setStripePromise] = useState(null);
    const [price, setPrice] = useState([]);
+
    useEffect(() => {
       const fetchConfig = async () => {
          try {
@@ -26,7 +27,7 @@ export default function Payment() {
 
             const { publishableKey, prices } = await response.json();
 
-            setStripePromise(loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY));
+            setStripePromise(loadStripe(publishableKey));
             setPrice(prices[0]);
          } catch (error) {
             return setMessages('Error fetching Stripe config:', error.message);
@@ -34,22 +35,25 @@ export default function Payment() {
       };
       fetchConfig();
    }, [])
-   if (!stripePromise) {
-      return <div>Loading...</div>;
+
+   if (!stripePromise || !price) {
+      return <div className="h-screen flex justify-center items-center font-comfortaa bg-white">
+         Cargando...
+      </div>;
    }
 
    return (
       <>
          <Navbar />
          <div className="font-comfortaa flex min-h-full flex-col items-center bg-white px-4 md:px-14 lg:px-20 pt-16">
-            <main className="w-full max-w-4xl flex flex-col gap-6">
+            <main className="w-full max-w-2xl flex flex-col gap-6">
                <div className="flex flex-col gap-3">
                   <h2 className="font-poppins font-semibold text-accent text-center text-xl md:text-3xl">Completar Pago</h2>
                   <p>Ingresa los detalles de tu tarjeta para procesar el pago</p>
                </div>
 
                <Elements stripe={stripePromise}>
-                  <PaymentForm
+                  <CheckoutForm
                      price={price}
                   />
                </Elements>
@@ -59,11 +63,11 @@ export default function Payment() {
    );
 }
 
-function PaymentForm({ price }) {
+function CheckoutForm({ price }) {
    const [messages, setMessages] = useState('');
 
    // Datos del cliente
-   const [fullName, setFullName] = useState('');
+   const [name, setName] = useState('');
    const [email, setEmail] = useState('');
    const [cardComplete, setCardComplete] = useState({
       cardNumber: false,
@@ -72,23 +76,64 @@ function PaymentForm({ price }) {
    });
 
    // Datos para crear transacción
-   const [customer, setCustomer] = useState(null);
+   const [customer_id, setCustomerId] = useState(null);
 
    // Datos de la transacción
-   const [clientSecret, setClientSecret] = useState(null);
-   const [subscriptionId, setSubscriptionId] = useState(null);
+   const [client_secret, setClientSecret] = useState(null);
 
    // Datos para realizar el pago
    const stripe = useStripe();
    const elements = useElements();
 
-   // 
+   // Evento para obtener datos de la tarjeta
    const handleCardChange = (event) => {
       setCardComplete({
          ...cardComplete,
          [event.elementType]: event.complete
       });
    };
+
+   // Obtener Cliente
+   const getCustomer = async () => {
+      if (customer_id) return customer_id;
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_END_POINT}/stripe/customer/create`, {
+         method: 'post',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({ email }),
+      });
+
+      if (!response.ok) {
+         return setMessages('Falló al obtener el cliente');
+      }
+
+      const data = await response.json();
+      setCustomerId(data.id);
+      return data.id;
+   }
+
+   // Obtener Suscripción
+   const getSubscription = async (customerId) => {
+      if (client_secret) return client_secret;
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_END_POINT}/stripe/subscription/create`, {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({ price: price.id, customer: customerId }),
+      });
+
+      if (!response.ok) {
+         return setMessages('Failed to fetch subscription');
+      }
+
+      const { clientSecret } = await response.json();
+      setClientSecret(clientSecret);
+      return clientSecret;
+   }
 
    const handleSubmit = async (e) => {
       e.preventDefault();
@@ -99,96 +144,76 @@ function PaymentForm({ price }) {
          return setMessages('Stripe has not loaded yet. Please try again.');
       }
 
+      if (!price) {
+         return setMessages('No hay un plan seleccionado. Por favor, seleccione un plan.');
+      }
+
       setMessages('')
 
+      // Validación de correo
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!emailRegex.test(email)) {
          return setMessages("El formato del correo no es válido");
       }
 
+      // Validación de datos de tarjeta
       if (!cardComplete.cardNumber || !cardComplete.cardExpiry || !cardComplete.cardCvc) {
          return setMessages("Por favor, complete todos los campos de la tarjeta.");
       }
 
       try {
          // Cliente
-         setCustomer(await getCustomer())
+         const customerId = await getCustomer();
 
          // Transacción
-         const subscription = await getSubscription()
-         if (!subscription.clientSecret || !subscription.subscriptionId) {
+         const clientSecret = await getSubscription(customerId);
+
+         if (!clientSecret) {
             return setMessages('Error: Payment intent not found');
          }
 
-         setSubscriptionId(subscription.subscriptionId);
-         setClientSecret(subscription.clientSecret);
+         setMessages('Esperando...');
 
          // Elementos de tarjeta
          const cardElement = elements.getElement(CardNumberElement);
          // Realizar Suscripción
-         const { error } = await stripe.confirmCardPayment(subscription.clientSecret, {
+         const { error } = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
-               card: elements.getElement(CardNumberElement),
-               billing_details: {
-                  name: fullName,
-               }
+               card: cardElement,
+               billing_details: { name }
             }
          });
 
-         if (error) {
-            return setMessages(error.message);
+         if (error) { // 
+            setMessages(`Payment failed: ${error.message}`);
+         } else {
+            setMessages('Payment successful! Se supone aquí debe mandar un modal y redireccionar al dashboard');
+
+            setName('')
+            setEmail('')
+
+            elements.getElement(CardNumberElement).clear();
+            elements.getElement(CardExpiryElement).clear();
+            elements.getElement(CardCvcElement).clear();
+
+            setCustomerId(null)
+            setClientSecret(null)
          }
 
       } catch (error) {
-         if (error) {
-            return setMessages(`Error: ${error.message}`);
-         }
+         return setMessages(`Error: ${error.message}`);
       }
 
    };
 
-   // Obtener Cliente
-   const getCustomer = async () => {
-      if (customer)
-         return customer
-
-      const resCustomer = await fetch(`${process.env.NEXT_PUBLIC_END_POINT}/stripe/customer/create`, {
-         method: 'post',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         body: JSON.stringify({ email }),
-      });
-
-      if (!resCustomer.ok) {
-         return setMessages('Falló al obtener el cliente');
-      }
-      return await resCustomer.json();
+   const currencyFormatter = ({ currency, value }) => {
+      const formatter = new Intl.NumberFormat('en-US', {
+         style: 'currency',
+         minimumFractionDigits: 2,
+         currency
+      })
+      return formatter.format(value)
    }
-
-   // Obtener Suscripción
-   const getSubscription = async () => {
-      if (subscriptionId)
-         return { subscriptionId, clientSecret }
-
-      const resSubscription = await fetch(`${process.env.NEXT_PUBLIC_END_POINT}/stripe/subscription/create`, {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         body: JSON.stringify({
-            price: price.id,
-            customer: customer.id
-         }),
-      });
-
-      if (!resSubscription.ok) {
-         return setMessages('Failed to fetch subscription');
-      }
-
-      return await resSubscription.json()
-   }
-
 
    const cardElementOptions = {
       style: {
@@ -219,8 +244,8 @@ function PaymentForm({ price }) {
                   type="text"
                   name="full_name"
                   placeholder="Escriba su nombre..."
-                  value={fullName}
-                  onChange={(e) => setFullName(e.target.value)}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   required />
             </div>
 
@@ -240,11 +265,11 @@ function PaymentForm({ price }) {
                <div className="rounded-full border border-gray-300 px-3 py-2 mb-2">
                   <CardNumberElement options={cardElementOptions} onChange={handleCardChange} />
                </div>
-               <div className="flex space-x-2">
-                  <div className="w-1/2 rounded-full border border-gray-300 px-3 py-2">
+               <div className="md:flex">
+                  <div className="w-full rounded-full border border-gray-300 px-3 py-2 mb-2">
                      <CardExpiryElement options={cardElementOptions} onChange={handleCardChange} />
                   </div>
-                  <div className="w-1/2 rounded-full border border-gray-300 px-3 py-2">
+                  <div className="w-full rounded-full border border-gray-300 px-3 py-2 mb-2">
                      <CardCvcElement options={cardElementOptions} onChange={handleCardChange} />
                   </div>
                </div>
@@ -252,11 +277,13 @@ function PaymentForm({ price }) {
 
             <div className="text-red-500 text-sm my-4">{messages}</div>
 
-            <Button
-               type="submit"
-            >
-               Pagar
-            </Button>
+            <div className="flex justify-center font-poppins">
+               <Button
+                  type="submit"
+               >
+                  Pagar {currencyFormatter({ currency: price.currency, value: price.unit_amount / 100 })} {price.currency.toUpperCase()}
+               </Button>
+            </div>
          </form>
       </div>
    );
