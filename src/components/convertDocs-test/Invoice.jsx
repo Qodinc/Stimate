74469d3c9c3e248ca15b98c6ccdcad98c4535c23
Santiago Yañez <angel.yaez2003@gmail.com>
2024-10-15 21:58:27 -0600

    .

--- src/components/convertDocs-test/Invoice.jsx
+++ src/components/convertDocs-test/Invoice.jsx
@@ -1,65 +1,72 @@
-import React from 'react';
-import { Page, Text, View, Document, StyleSheet, Font } from '@react-pdf/renderer';
+import React from "react";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
 
 // Registrar fuentes personalizadas si es necesario
 Font.register({
-  family: 'Roboto',
-  src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf'
+  family: "Roboto",
+  src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf",
 });
 
 const styles = StyleSheet.create({
-  page: { 
+  page: {
     padding: 30,
-    fontFamily: 'Roboto',
-    backgroundColor: '#ffffff'
+    fontFamily: "Roboto",
+    backgroundColor: "#ffffff",
   },
   header: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 20,
-    justifyContent:'space-between',
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 28,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    color: '#112131'
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "#112131",
   },
   project: {
     fontSize: 24,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    color: '#112131',
-    textAlign:'right', 
-    flexDirection:'column'
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "#112131",
+    textAlign: "right",
+    flexDirection: "column",
   },
   text: {
-    fontSize:12,
-    fontWeight:'bold',
-    flexDirection:'column', 
+    fontSize: 12,
+    fontWeight: "bold",
+    flexDirection: "column",
   },
   logo: {
     width: 74,
     height: 66,
-    marginLeft: 'auto',
+    marginLeft: "auto",
     marginRight: 0,
   },
   invoiceInfo: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 20,
     marginBottom: 40,
   },
   dateSection: {
-    marginBottom:30,
-    flexDirection:'column'
+    marginBottom: 30,
+    flexDirection: "column",
   },
   invoiceDetails: {
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   label: {
     fontSize: 12,
-    fontWeight: 'bold',
-    color: '#112131',
+    fontWeight: "bold",
+    color: "#112131",
     marginBottom: 5,
   },
   value: {
@@ -67,44 +74,32 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   table: {
-    display: 'table',
-    width: 'auto',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderColor: '#bfbfbf',
-    borderRightWidth: 0,
-    borderBottomWidth: 0,
+    display: "table",
+    width: "auto"
   },
   tableRow: {
-    margin: 'auto',
-    flexDirection: 'row',
+    margin: "auto",
+    flexDirection: "row",
   },
   tableColHeader: {
-    width: '25%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderColor: '#bfbfbf',
-    borderLeftWidth: 0,
-    borderTopWidth: 0,
-    backgroundColor: '#f0f0f0',
+    width:"auto",
+    borderBottomWidth: 2,
+    borderBottomColor: "#112131",
+    borderBottomStyle: "solid",
   },
   tableCol: {
-    width: '25%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderColor: '#bfbfbf',
-    borderLeftWidth: 0,
-    borderTopWidth: 0,
+    width: "25%",
+    flexDirection:"column"
   },
   tableCellHeader: {
-    margin: 'auto',
+    margin: "auto",
     marginTop: 5,
     marginBottom: 5,
     fontSize: 12,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   tableCell: {
-    margin: 'auto',
+    margin: "auto",
     marginTop: 5,
     marginBottom: 5,
     fontSize: 10,
@@ -112,8 +107,8 @@ const styles = StyleSheet.create({
   total: {
     marginTop: 30,
     fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'right',
+    fontWeight: "bold",
+    textAlign: "right",
   },
 });
 
@@ -125,10 +120,10 @@ const Invoice = ({ invoiceData }) => (
         <Text style={styles.project}>Tacos Pepe</Text>
         {/* Aquí puedes agregar un logo si lo deseas */}
         {/* <Image style={styles.logo} src="/path-to-your-logo.png" /> */}
-      </View> 
+      </View>
       <View style={styles.dateSection}>
-          <Text style={styles.text}>Fecha: {invoiceData.date}</Text>
-          <Text style={styles.text}>No: {invoiceData.invoiceNumber}</Text>
+        <Text style={styles.text}>Fecha: {invoiceData.date}</Text>
+        <Text style={styles.text}>No: {invoiceData.invoiceNumber}</Text>
       </View>
 
       <View style={styles.invoiceInfo}>
@@ -144,8 +139,17 @@ const Invoice = ({ invoiceData }) => (
         </View>
       </View>
 
+      <View>
+        <View style={styles.tableColHeader}>
+          <Text style={styles.text}>Detalles</Text>
+        </View>
+      </View>
+
       <View style={styles.table}>
         <View style={styles.tableRow}>
+          <View style={styles.tableColHeader}>
+            <Text style={styles.tableCellHeader}>Producto</Text>
+          </View>
           <View style={styles.tableColHeader}>
             <Text style={styles.tableCellHeader}>Descripción</Text>
           </View>
@@ -153,12 +157,16 @@ const Invoice = ({ invoiceData }) => (
             <Text style={styles.tableCellHeader}>Cantidad</Text>
           </View>
           <View style={styles.tableColHeader}>
+            <Text style={styles.tableCellHeader}>Unidad</Text>
+          </View>
+          <View style={styles.tableColHeader}>
             <Text style={styles.tableCellHeader}>Precio Unitario</Text>
           </View>
           <View style={styles.tableColHeader}>
-            <Text style={styles.tableCellHeader}>Total</Text>
+            <Text style={styles.tableCellHeader}>Precio</Text>
           </View>
         </View>
+        
         {invoiceData.items.map((item, index) => (
           <View style={styles.tableRow} key={index}>
             <View style={styles.tableCol}>
@@ -172,6 +180,9 @@ const Invoice = ({ invoiceData }) => (
             </View>
             <View style={styles.tableCol}>
               <Text style={styles.tableCell}>${item.total.toFixed(2)}</Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>${item.unitPrice.toFixed(2)}</Text>
             </View>
           </View>
         ))}
