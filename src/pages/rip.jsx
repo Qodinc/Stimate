2230572830485532908dc4c1e628d68d34e16d48
Manuel Cruz <amcp.negocios@gmail.com>
2024-11-09 22:23:26 -0600

    rip

---
+++ src/pages/rip.jsx
@@ -0,0 +1,9 @@
+ export default function RipSantiago() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">       
+            <img src="Icons/ripsantiago.jpg" alt="santiago" className="object-cover w-[25%] rounded-full" />
+            <h1 className="text-4xl font-bold text-accent">
+                RIP Santiago
+            </h1>
+        </div>
+    )}
