22961ddd11738d1eb4627eb4d49738f4db1cc7b5
Manuel Cruz <amcp.negocios@gmail.com>
2024-11-21 19:47:14 -0600

    feat: Updated save button, deleted top one, created Ir a Dashboard

---
+++ src/components/DashboardButton.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Home } from "lucide-react";
+
+const DashboardButton = () => {
+  const { pathname } = useRouter();
+
+  // Ocultar botón si estás en el dashboard
+  if (pathname === "/") return null;
+
+  return (
+    <Link
+      href="/dashboard"
+      className="ml-2 md:ml-10 font-bold flex items-center place-self-center text-white bg-accent py-2 px-3 rounded-full cursor-pointer hover:bg-white justify-between gap-2 hover:text-accent transition-colors duration-200"
+    >
+      <Home className="w-6 h-6" />
+      <span className="hidden md:block">Ir a <span>Inicio</span></span> 
+    </Link>
+  );
+};
+
+export default DashboardButton;
