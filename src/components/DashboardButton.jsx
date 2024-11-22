ed6ed0d2aa4ee9f0b892803ac88423102aea0bb0
Manuel Cruz <amcp.negocios@gmail.com>
2024-11-21 21:03:06 -0600

    hotfix /

--- src/components/DashboardButton.jsx
+++ src/components/DashboardButton.jsx
@@ -10,7 +10,7 @@ const DashboardButton = () => {
 
   return (
     <Link
-      href="/dashboard"
+      href="/"
       className="ml-2 md:ml-10 font-bold flex items-center place-self-center text-white bg-accent py-2 px-3 rounded-full cursor-pointer hover:bg-white justify-between gap-2 hover:text-accent transition-colors duration-200"
     >
       <Home className="w-6 h-6" />
